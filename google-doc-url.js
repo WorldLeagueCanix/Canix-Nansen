e7054032e2b8440ef532a2a367bdd6b0ec68939b
WorldLeagueCanix <142015114+WorldLeagueCanix@users.noreply.github.com>
2023-08-12 08:31:19 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1LYO63eWA_dervcUDgRiAwQaMvmTY2Yo03CuUsr1SC2Y/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'No-API-Key';
